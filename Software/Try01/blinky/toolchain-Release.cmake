7585e0ff774218eea268d2364f13935b9c919156
Jedzia <jed69@gmx.de>
2019-12-21 16:44:54 +0100

    a CMake approach

---
+++ Software/Try01/blinky/toolchain-Release.cmake
@@ -0,0 +1,3 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_C_COMPILER "C:/Toolchain/ARM/GNU MCU Eclipse/ARM Embedded GCC/9-2019-q4-major-win32/bin/arm-none-eabi-gcc.exe")
+SET(CMAKE_CXX_COMPILER "C:/Toolchain/ARM/GNU MCU Eclipse/ARM Embedded GCC/9-2019-q4-major-win32/bin/arm-none-eabi-g++.exe")
