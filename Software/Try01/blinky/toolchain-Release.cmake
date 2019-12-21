472b73c544880a4f11763faa9245d13478f265f1
Jedzia <jed69@gmx.de>
2019-12-21 16:55:47 +0100

    unneeded

--- Software/Try01/blinky/toolchain-Release.cmake
+++
@@ -1,3 +0,0 @@
-SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_C_COMPILER "C:/Toolchain/ARM/GNU MCU Eclipse/ARM Embedded GCC/9-2019-q4-major-win32/bin/arm-none-eabi-gcc.exe")
-SET(CMAKE_CXX_COMPILER "C:/Toolchain/ARM/GNU MCU Eclipse/ARM Embedded GCC/9-2019-q4-major-win32/bin/arm-none-eabi-g++.exe")
