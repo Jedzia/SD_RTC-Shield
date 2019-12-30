3699dc608a160f2a5651a0c71f223e5b26b00330
Jedzia <jed69@gmx.de>
2019-12-30 15:13:47 +0100

    THIS FUCKING SHIT: mbed 2 and v5 confusion

--- Software/Try03PlatformIO/src/main.cpp
+++ Software/Try03PlatformIO/src/main.cpp
@@ -25,14 +25,13 @@
 
 //#include "filesystem/fat/FATFileSystem.h"
 
-#include "BlockDevice.h"
+//#include "BlockDevice.h"
 // Maximum number of elements in buffer
 #define BUFFER_MAX_LEN 10
 
 // This will take the system's default block device
-BlockDevice *bd = BlockDevice::get_default_instance();
+//BlockDevice *bd = BlockDevice::get_default_instance();
 #include "SDFileSystem.h"
-SDFileSystem sd(PA_7, PA_6, PA_5, PB_6, "sd"); // the pinout on the mbed Cool Components workshop board
 
 //#include "FATFileSystem.h"
 //FATFileSystem fs("fs");
@@ -62,7 +61,7 @@ int main() {
     RtcDs1307 rtc(i2c1);
 
     if(!rtc.isRunning()) {
-        printf("RTC 1 is NOT running!");
+        usart2.printf("RTC 1 is NOT running!");
         // following line sets the RTC to the date & time this sketch was compiled
         // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
         // This line sets the RTC with an explicit date & time, for example to set
@@ -72,7 +71,7 @@ int main() {
 
     DateTime dt = rtc.now();                     // check clock value
     if(dt.year() > 2090 || dt.year() < 2014) {
-        printf("Setting up time\n");
+        usart2.printf("Setting up time\n");
         DateTime compiled(__DATE__, __TIME__);      // to set RT clock initially
         compiled = DateTime(compiled.unixtime() + 10);
         rtc.adjust(compiled);
@@ -82,7 +81,7 @@ int main() {
     //set_time(0);                        // set active clock
 
     if(!rtc.isRunning()) {
-        printf("RTC 2 is NOT running!");
+        usart2.printf("RTC 2 is NOT running!");
         // following line sets the RTC to the date & time this sketch was compiled
         // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
         // This line sets the RTC with an explicit date & time, for example to set
@@ -90,27 +89,45 @@ int main() {
         // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
     }
 
-
+    SDFileSystem sd(PA_7, PA_6, PA_5, PB_6, "sd"); // the pinout on the mbed Cool Components workshop board
 
     DIR *dp;
     struct dirent *dirp;
+    auto sectors = sd.disk_sectors();
+    usart2.printf("sd.disk_sectors:%lu\n", sectors);
+    auto status = sd.disk_status();
+    usart2.printf("sd.status:%d\n", status);
+
+    auto init = sd.disk_initialize();
+    usart2.printf("sd.init:%d\n", init);
+
+    sectors = sd.disk_sectors();
+    usart2.printf("sd.disk_sectors:%lu\n", sectors);
+    status = sd.disk_status();
+    usart2.printf("sd.status:%d\n", status);
+
+    
     dp = opendir("/sd");
-    auto xxx = dp->size();
-    printf("dp size:%d\n", 5);
+    if(dp == nullptr) {
+        usart2.printf("Failed to open SD Card.");
+    } else {
+        auto xxx = dp->size();
+        usart2.printf("dp size:%d\n", 5);
 
-    //int err = fs.mount(bd);
-    //printf("%s\n", (err ? "Fail :(" : "OK"));
+        //int err = fs.mount(bd);
+        //printf("%s\n", (err ? "Fail :(" : "OK"));
 
-    /*mkdir("/sd/mydir", 0777);
+        /*mkdir("/sd/mydir", 0777);
 
-    FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
-    if(fp == nullptr) {
-        error("Could not open file for write\n");
+        FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
+        if(fp == nullptr) {
+            error("Could not open file for write\n");
+        }
+        fprintf(fp, "Hello fun SD Card World!");
+        fclose(fp);
+
+        printf("Goodbye World!\n");*/
     }
-    fprintf(fp, "Hello fun SD Card World!");
-    fclose(fp);
-
-    printf("Goodbye World!\n");*/
 
 
     while(true) {
