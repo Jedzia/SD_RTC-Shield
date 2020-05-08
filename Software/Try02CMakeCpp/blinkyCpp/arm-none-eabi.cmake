553466b7db04722d8e937a4efa65c60e7cbd64c5
Jedzia <jed69@gmx.de>
2020-05-08 14:59:24 +0200

    compiles but fails on linking

--- Software/Try02CMakeCpp/blinkyCpp/arm-none-eabi.cmake
+++ Software/Try02CMakeCpp/blinkyCpp/arm-none-eabi.cmake
@@ -2,6 +2,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 SET(CMAKE_CROSSCOMPILING 1)
 #set(CMAKE_C_COMPILER_WORKS 1) # In some cases you have to force compiler detection to work.
+#set(CMAKE_CXX_COMPILER_WORKS 1) # In some cases you have to force compiler detection to work.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY) # ToDo: a way to specify different link flags for TryCompile
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -11,7 +12,10 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(OBJCOPY arm-none-eabi-objcopy)
 set(SIZE arm-none-eabi-size)
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+#set(CMAKE_ASM_COMPILER arm-none-eabi-as)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_C_COMPILER   arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
 #Enable semi hosting
 #set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=rdimon.specs")
