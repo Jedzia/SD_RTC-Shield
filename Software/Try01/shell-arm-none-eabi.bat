2a7cc3960abfd5b33bd95ad9c86075fa19f35a78
Jedzia <jed69@gmx.de>
2020-05-27 01:36:23 +0200

    added configuration like in Try03PlatformIO

--- Software/Try01/shell-arm-none-eabi.bat
+++ Software/Try01/shell-arm-none-eabi.bat
@@ -1,33 +1,7 @@
-@title Launching Clion with 'ARM Embedded GCC\9-2019-q4-major-win32\bin' settings.
+@title 'ARM Embedded GCC\9-2019-q4-major-win32\bin' Shell.
 @echo off
-REM set PATH=C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\bin;C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\libexec\gcc\mips-unknown-linux-gnu\4.9.2;%PATH%
-REM set PATH=C:\Toolchain\ARM\GNU Tools ARM Embedded\8 2018-q4-major\bin;C:\Toolchain\mingw-w64\x86_64-6.2.0-posix-seh-rt_v5-rev1\mingw64\bin;C:\msys64\usr\bin;%PATH%
-REM set PATH=C:\Toolchain\ARM\GNU Tools ARM Embedded\8 2018-q4-major\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-REM set PATH=C:\Toolchain\ARM\gcc-arm-none-eabi-4_8-140708\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-REM set PATH=D:\Users\Jedzia.pubsiX\AppData\Local\Arduino15\packages\arduino\tools\arm-none-eabi-gcc\4.8.3-2014q1\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-REM set PATH=C:\Toolchain\ARM\GNU MCU Eclipse\ARM Embedded GCC\8.2.1-1.4-20190214-0604\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-set PATH=C:\Toolchain\ARM\GNU MCU Eclipse\ARM Embedded GCC\9-2019-q4-major-win32\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
+call %~dp0%config\config.bat
 
-REM call C:\Toolchain\ARM\GNU MCU Eclipse\ARM Embedded GCC\9-2019-q4-major-win32\bin\gccvar.bat
+type %~dp0%config\WelcomeShell.txt
 
-REM set TOOL_PATH=/C/Toolchain/ARM/GNU Tools ARM Embedded/8 2018-q4-major/bin
-
-REM set CC=arm-none-eabi-gcc
-REM set CFLAGS_arm-none-eabi=-EL -mips32 -msoft-float
-REM set AR_arm-none-eabi=arm-none-eabi-ar
-
-REM set OPENSSL_DIR=C:\Toolchain\MIPS\openssl\usr
-
-REM set MAKEFLAGS=-j8
-
-REM call C:\D\ldc2vars64.bat
-rem e:
-rem cd E:\Projects\D
-
-echo .
-echo .
-echo cargo build --target=arm-none-eabi
-echo xargo build --target=arm-none-eabi
-echo cargo build --target=x86_64-pc-windows-gnu
-echo .
 cmd
--- Software/Try01/start-clion-arm-none-eabi.bat
+++ Software/Try01/start-clion-arm-none-eabi.bat
@@ -1,30 +1,10 @@
 @title Launching Clion with 'ARM Embedded GCC\9-2019-q4-major-win32\bin' settings.
 @echo off
-REM set PATH=C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\bin;C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\libexec\gcc\mips-unknown-linux-gnu\4.9.2;%PATH%
-REM set PATH=C:\Toolchain\ARM\GNU Tools ARM Embedded\8 2018-q4-major\bin;C:\Toolchain\mingw-w64\x86_64-6.2.0-posix-seh-rt_v5-rev1\mingw64\bin;C:\msys64\usr\bin;%PATH%
-REM set PATH=C:\Toolchain\ARM\GNU Tools ARM Embedded\8 2018-q4-major\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-REM set PATH=C:\Toolchain\ARM\gcc-arm-none-eabi-4_8-140708\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-REM set PATH=D:\Users\Jedzia.pubsiX\AppData\Local\Arduino15\packages\arduino\tools\arm-none-eabi-gcc\4.8.3-2014q1\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-REM set PATH=C:\Toolchain\ARM\GNU MCU Eclipse\ARM Embedded GCC\8.2.1-1.4-20190214-0604\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
-set PATH=C:\Toolchain\ARM\GNU MCU Eclipse\ARM Embedded GCC\9-2019-q4-major-win32\bin;C:\Toolchain\mingw-w64\x86_64-w64-mingw32-8.3.0-crt-6.0.0-multilib\bin;C:\msys64\usr\bin;C:\Toolchain\OpenOCD\OpenOCD-20190210-0.10.0\bin;%PATH%
+call %~dp0%config\config.bat
 
-REM call C:\Toolchain\ARM\GNU MCU Eclipse\ARM Embedded GCC\9-2019-q4-major-win32\bin\gccvar.bat
-
-REM set TOOL_PATH=/C/Toolchain/ARM/GNU Tools ARM Embedded/8 2018-q4-major/bin
-
-REM Used by gdb to refer to your master C:\Users\<USER>\.gdbinit
 set HOME=%USERPROFILE%
 
-REM set CC=arm-none-eabi-gcc
-REM set CFLAGS_arm-none-eabi=-EL -mips32 -msoft-float
-REM set AR_arm-none-eabi=arm-none-eabi-ar
-
-REM set MAKEFLAGS=-j8
-
-REM call C:\D\ldc2vars64.bat
-rem cd E:\Projects\D
-
-start "" "C:\Program Files\JetBrains\CLion 2019.2.3\bin\clion64.exe"
+start "" "C:\Program Files\JetBrains\CLion\bin\clion64.exe"
 
 @REM sleep 8 seconds
 @PING localhost -n 9 >NUL
