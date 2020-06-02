89df1c89e42c061691bf052a400377d2978ac1bc
Jedzia <jed69@gmx.de>
2020-06-02 23:19:47 +0200

    added msys runner

---
+++ Software/Try02CMake/shell-arm-none-eabi-msys2.bat
@@ -0,0 +1,7 @@
+@title 'ARM Embedded GCC\9-2019-q4-major-win32\bin MSys2 Shell.'
+@echo off
+call %~dp0%config\config.bat
+
+set CMAKE_GENERATOR=MSYS Makefiles
+
+C:\msys64\msys2_shell_zsh.cmd -use-full-path -here -mingw64 -c "cat config/WelcomeShell.txt ; cd blinky/build ; zsh"
